5dbc21c85c01f470f6dee5f1280598d9bece2394
Brad Alexander <brad.alexander913@gmail.com>
2019-08-15 01:28:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,11 +2,10 @@ def reverse_each_word(str)
   str_array = []
   rev_str_array = []
   str_array = str.split(" ")
-  str_array.each do |word|
+  str_array.collect do |word|
     rev_str_array << word.reverse
   end
   rev_str = rev_str_array.join(" ")
-  rev_str 
 end
 
   
